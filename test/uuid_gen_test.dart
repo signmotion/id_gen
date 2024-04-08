672550e46292ae5f6c89e8bdaf80618b355bc194
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-08 11:15:02 +0200

    Reorganized code.

--- test/uuid_gen_test.dart
+++ test/uuid_gen_test.dart
@@ -2,10 +2,9 @@ import 'package:id_gen/id_gen.dart';
 import 'package:test/test.dart';
 
 void main() {
-  const gen = UuidGen();
-
   group('generate', () {
     test('many unique UUIDs', () {
+      const gen = UuidGen();
       final generated = <String>{};
       for (var i = 0; i < 1200000; ++i) {
         final id = gen.next;
